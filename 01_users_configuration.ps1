bba9e3b29ac050c5869f696d5b4fd4e0deac4454
Fisstouk <90407928+Fisstouk@users.noreply.github.com>
2024-06-27 23:20:00 +0200

    feat(users): add user configuration script, need to test again

---
+++ 01_users_configuration.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+    Creating users in an Active Directory. Only used for 
+
+.DESCRIPTION
+    This script creates three users and ask the administrator to enter their
+    password. For education purposes, an SPN is added, pre-authentication is
+    deactivated and Generic rights are added.
+
+.EXAMPLE
+    .\01_users_configuration.ps1
+#>
+
+[String[]] $UserList = @(
+    'u_kerberoast',
+    'u_asreproast',
+    'u_generic'
+)
+
+foreach ($Item in $UserList) {
+
+        $SecurePassword = Read-Host "Enter a password for the user ` 
+        | $UserList[$Index]: " -AsSecureString -MaskInput 
+        New-ADUser -Name $UserList[$Index] -AccountPassword $SecurePassword `
+        | -Enabled $True
+
+}
+
+# Add SPN to u_kerberoast user
+
+Set-ADUser "$UserList[0]" `
+| -ServicePrincipalNames @{Add="HTTP/FAKE01.ADTEST.LOCAL"}
+
+# Deactivate pre-authentication for u_asreproas user
+
+Set-ADAccountControl -DoesNotRequirePreAuth $True -Identity $UserList[1]
+
+# Add GenericAll rights to u_generic user 
+
+Add-ADPermission -User $UserList[3] -ExtendedRights 'GenericAll'
