bfd579fbaf3de1f76ba27e369842c8b158c9804e
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-06-28 09:46:54 +0200

    doc: add header comment

--- 03_ad_vuln_conf.ps1
+++ 03_ad_vuln_conf.ps1
@@ -1,3 +1,16 @@
+# Name          : 03_ad_vuln_conf.ps1
+# Description   : Configure in a vulnerable way 3 users
+# Param 1       : N/A
+# Param 2       : N/A
+#
+# Exemple       : .\02_ad_installation.ps1
+#
+# Author        : Mathis THOUVENIN, Raphaël KATHALUWA-LIYANAGE, Lyronn LEVY
+# Changelog     :
+# Version       : 0.3.1
+#
+#
+
 # creating the users
 net.exe user u_kerberoast "P@ssw0rd" /add /domain
 net.exe user u_asreproast "P@ssw0rd" /add /domain
