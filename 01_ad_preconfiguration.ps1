30a19ff2b4f579a115e70389a96fbfd9ad9a21ce
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-06-29 11:38:09 +0200

    chore: rename file for semantic

--- 01_ad_preconfiguration.ps1
+++ 01_ad_preconfiguration.ps1
@@ -1,13 +1,13 @@
-# Name          : 01_preconfiguration.ps1
+# Name          : 01_ad_preconfiguration.ps1
 # Description   : Configure IP Address and Computer Name
 # Param 1       : N/A
 # Param 2       : N/A
 #
-# Exemple       : .\01_preconfiguration.ps1
+# Exemple       : .\01_ad_preconfiguration.ps1
 #
 # Author        : Mathis THOUVENIN, Raphaël KATHALUWA-LIYANAGE, Lyronn LEVY
 # Changelog     :
-# Version       : 1
+# Version       : 1.0.1
 #
 #
 
@@ -21,7 +21,7 @@ $dns = "192.168.1.52,192.168.1.1"
 ### IP configuration
 # get interface name
 Get-NetAdapter | Select-Object ifIndex, Name, Description, MacAddress, Status
-$interface_name = Read-Host "Select the Name of the interface to configure:     "
+$interface_name = Read-Host "Select the Name of the interface to configure"
 
 # Remove the static ip
 Remove-NetIPAddress -InterfaceAlias $interface_name -Confirm:$false
@@ -39,7 +39,7 @@ Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6 -Confirm:$false
 # choose to either rename or rename and join a domain in one go
 $renaming = 0
 while($renaming -ne 1) {
-    $choice = Read-Host "Would you like to join a domain? (y/n):    "
+    $choice = Read-Host "Would you like to join a domain? (y/n)"
     switch ($choice) {
         "n" {Rename-Computer -ComputerName $env:COMPUTERNAME -NewName $computer_name -Restart; Break}
         "y" {Add-Computer -ComputerName $env:COMPUTERNAME -DomainName $domain -NewName $computer_name -Restart; Break}
